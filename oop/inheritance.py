28600ba2a5358eaad168ca0fb95e1fe0ebde0a91
Yuki Kitayama <yk2797@columbia.edu>
2023-05-18 08:17:35 -0700

    learning inheritance

--- oop/inheritance.py
+++ oop/inheritance.py
@@ -14,3 +14,43 @@ for cls1 in [Vehicle, LandVehicle, TrackedVehicle]:
     for cls2 in [Vehicle, LandVehicle, TrackedVehicle]:
         print(cls1.__name__ + " is subclass of " + cls2.__name__ + ": ", issubclass(cls1, cls2))
 
+
+class Super:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return "My name is " + self.name + "."
+
+
+class Sub(Super):
+    def __init__(self, name):
+        # Super.__init__(self, name)
+        super().__init__(name)
+
+
+obj = Sub("Yuki")
+print(obj)
+
+
+class Dog:
+    kennel = 0
+    def __init__(self, breed):
+        self.breed = breed
+        Dog.kennel += 1
+    def __str__(self):
+        return self.breed + " says: Woof!"
+
+
+class SheepDog(Dog):
+    def __str__(self):
+        return super().__str__() + " Don't run away, Little Lamb!"
+
+
+class LowlandDog(SheepDog):
+    def __str__(self):
+        return Dog.__str__(self) + " I don't like mountains!"
+
+
+l = LowlandDog("test")
+print(l)
