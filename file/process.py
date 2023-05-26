f9a859af7c3bd5b46da07897bdffe7ad18393fc6
Yuki Kitayama <yk2797@columbia.edu>
2023-05-26 09:06:55 -0700

    learning file processing with Python

--- file/process.py
+++ file/process.py
@@ -1,5 +1,18 @@
+from os import strerror
 
+try:
+    cnt = 0
+    stream = open("text.txt", "rt", encoding="utf-8")
+    ch = stream.read(1)
 
-stream = open("text.txt", "rt", encoding="utf-8")
+    while ch != "":
+        print(ch, end="")
+        cnt += 1
+        ch = stream.read(1)
 
-print(stream.read())
+    stream.close()
+
+    print("\n\nCharacters in file: ", cnt)
+
+except IOError as e:
+    print("I/O error occurred: ", strerror(e.errno))
