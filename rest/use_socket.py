f63a1f6b36c261ab9b821999df0c21042f7e333c
Yuki Kitayama <yk2797@columbia.edu>
2023-06-22 07:24:30 -0700

    test socket get

---
+++ rest/use_socket.py
@@ -0,0 +1,12 @@
+import socket
+
+
+server_addr = input("What server do you want to connect to? ")
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((server_addr, 80))
+
+sock.send(
+    b"GET / HTTP/1.1\r\nHost: "
+    + bytes(server_addr, "utf8")
+    + b"\r\nConnection: close\r\n\r\n"
+)
