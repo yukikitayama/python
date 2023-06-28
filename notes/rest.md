13e8b13294520a489e9d460d9884d66e132f5d9e
Yuki Kitayama <yk2797@columbia.edu>
2023-06-28 09:55:12 -0700

    crud with requests

--- notes/rest.md
+++ notes/rest.md
@@ -1,5 +1,9 @@
 # REST
 
+## Request
+
+`?` in the request URI separates the resource identification from additional request parameters.
+
 ## Response
 
 Consists of header and contents
@@ -15,4 +19,10 @@ Consists of header and contents
 Needs server's address and the service port number. Default 80 if the port number is omitted.
 
 `text` property stores the raw response's contents as string.
+
+Response method `json()` can be used if the server response's header `Content-Type` contains `application/json`.
+
+## CRUD
+
+Create (POST), Read (GET), Update (PUT), Delete (DELETE)
 
---
+++ rest/crud.py
@@ -0,0 +1,39 @@
+import requests
+
+names = ["id", "brand", "model", "production_year", "convertible"]
+widths = [10, 15, 10, 20, 15]
+
+
+def show_head():
+    for n, w in zip(names, widths):
+        print(n.ljust(w), end="| ")
+    print()
+
+
+def show_car(car):
+    for n, w in zip(names, widths):
+        print(str(car[n]).ljust(w), end="| ")
+    print()
+
+
+def show(json_):
+    show_head()
+    for car in json_:
+        show_car(car)
+
+
+try:
+    reply = requests.get("http://localhost:3000/cars")
+except requests.RequestException:
+    print("Communication error")
+else:
+    if reply.status_code == requests.codes.ok:
+        # print(reply.text)
+        print(reply.headers["Content-Type"])
+        # print(reply.json())
+        show(reply.json())
+    elif reply.status_code == requests.codes.not_found:
+        print("Resource not found")
+    else:
+        print("Server error")
+
